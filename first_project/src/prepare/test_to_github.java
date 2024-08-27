e73608ebd6b5a64ffab04ae149561324b9ff2c4f
zijie <zijie@192.168.42.24>
2024-08-26 23:36:42 -0700

    second push to local git repository: users.zijie.git.repository:first_project

--- first_project/src/prepare/test_to_github.java
+++ first_project/src/prepare/test_to_github.java
@@ -5,6 +5,7 @@ public class test_to_github {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello usf! my friend"); // 输出 Hello
+        System.out.println("Hello usf! my friend modify and push"); // 输出 Hello and push to local git repo
 	}
 
 }
