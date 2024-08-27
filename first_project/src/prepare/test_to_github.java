7a3028271f8d34599609cbdadfa3158124956713
zijie <zijie@192.168.42.24>
2024-08-26 23:12:38 -0700

    first_try to connect to github. 1Go to github create new repository, 2and Copy http URL from github 3Go to Eclipse --> Right click on project --> Team--> share project--> Create new Git Repo. 4Go to Git Staging--> add to index --> commit and push.(-->this step)
    5Right click on Git Repositories push to up stream
    6Paste your http URL of github which you copy in step 2
    7Enter username and password of github -- > Finish

---
+++ first_project/src/prepare/test_to_github.java
@@ -0,0 +1,10 @@
+package prepare;
+
+public class test_to_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello usf! my friend"); // 输出 Hello
+	}
+
+}
